ffdf99732a60cb21fa09c60c66d812d2455b3b9d
CalderMarshall <cmarshall72@gmail.com>
2017-03-04 18:34:47 -0500

    comments in box program

--- box.js
+++ box.js
@@ -1,3 +1,4 @@
+//  RANGE FUNCTION
 function range(min, max) {
   var arr = [];
   for (var i = min; i < max; i++) {
@@ -5,15 +6,32 @@ function range(min, max) {
   }
   return arr;
 }
-
+//  MAIN BOX FUNCTION
 var box = function (width, height) {
-  var arr = range(0 , width);
-  letter = function(){
+     // SETTING SKELETON ARRAYS FOR WIDTH AND HEIGHT OF ASTERISK BOX, WHICH WILL BE PASSED IN FUNCTION
+  var arr_width = range(0 , width);
+  var arr_height = range(0,height);
+
+
+          // MAPPING NUMBERS INTO STARS
+  var arr_stars = arr_width.map(function (x) {
     return "*";
-  };
 
+  });
+  // REDUCING STARS TO ONE STRING
+  var arr_reduced = arr_stars.reduce(function (x, y) {
+    return x + y;
+  });
+  // PRITING SET OF REDUCED  STARS WITH FOREACH BY AMOUNT OF HEIGHT PARAMETER
+  arr_height.forEach(function(x){
+    console.log(arr_reduced);
+  });
 
-  var arr_new = arr.map(letter);
-  return arr_new;
+  // OR RUN THIS WITH THE MAP FUNCTION, AND RETURN ARR_NEW2
+
+  // var arr_new2 = arr_height.map(function(x){
+  // return arr_reduced;
+  // });
+  return arr_reduced;
 };
-console.log(box(5, 5));
+console.log(box(50, 20));
